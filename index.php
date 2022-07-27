0878ac8de16872758d74e67647f4e5d6f18dfadb
Danyoshi <danyoshi@gmail.com>
2022-07-27 15:55:19 -0300

    page class

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php 
 
 require_once("vendor/autoload.php");
+require_once("vendor/hcodebr/php-classes/src/page.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+//use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new \Hcode\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
-
+	$page->setTpl("index");
 });
 
 $app->run();
