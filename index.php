cb625c1d22ebd6608a1b0142c3fe4975d0c55303
Danyoshi <danyoshi@gmail.com>
2022-07-28 14:03:18 -0300

    template admin css bagunçado

--- index.php
+++ index.php
@@ -2,17 +2,28 @@
 
 require_once("vendor/autoload.php");
 require_once("vendor/hcodebr/php-classes/src/page.php");
+require_once("vendor/hcodebr/php-classes/src/pageAdmin.php");
 
-use \Slim\Slim;
-//use \Hcode\Page;
+use Slim\Slim;
+//use Ecommerce\Pagina;
+
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 });
--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -1,7 +1,7 @@
 <?php
-//namespace Hcode;
+//namespace Ecommerce;
 
-use Rain\Tpl;
+use \Rain\Tpl;
 
 class Page {
 
@@ -12,12 +12,12 @@ class Page {
     ];
 
 
-    public function __construct($opts = array()) {
+    public function __construct($opts = array(), $tpl_dir = "/views/") {
 
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
-            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'] . "/views/",
+            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'] . $tpl_dir,
             "cache_dir"     => $_SERVER['DOCUMENT_ROOT'] . "/views-cache/",
             "debug"         => false 
         );
